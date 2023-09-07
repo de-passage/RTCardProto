d0f40b0b63543fa30d6eb8fa9831bf47fb17ab65
de-passage <contact@sylvainleclercq.com>
2023-09-07 18:29:43 +0200

    Remove worst wound on rest

--- Scenes/rest.gd
+++ Scenes/rest.gd
@@ -5,13 +5,15 @@ var _heal_value: int = 0
 @onready var _card_removal = $MarginContainer
 
 func _ready():
+	_rest_button.disabled = _worst_wound() == null
 	var player = Global.get_player()
 	_heal_value = player.max_hp / 4
 	print(str(_heal_value))
-	_rest_button.text = "Heal for %s" % _heal_value
-
+	
 func _on_rest_button_pressed():
-	Global.current_health += _heal_value
+	var worst_wound = _worst_wound()
+	if worst_wound != null: 
+		Global.remove_from_deck(worst_wound)	
 	get_tree().change_scene_to_file("res://Scenes/path_selection.tscn")
 
 func _on_remove_button_pressed():
@@ -24,3 +26,20 @@ func _on_deck_panel_card_removed(card: CardDeckInstance):
 
 func _on_deck_panel_closed():
 	_card_removal.visible = false
+
+
+func _worst_wound():
+	var worst = null
+	var max: int = 0
+	for card in Global.get_current_deck(): 
+		for status in card.statuses():
+			if status is Wound: 
+				var v = status.get_value()
+				if v > max: 
+					max = v
+					worst = card
+	return worst
+
+
+func _on_ignore_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/path_selection.tscn")
